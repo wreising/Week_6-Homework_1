981bba46d26bcb3a4750f3a9e7c2a1416244e876
wreising <wreising@mac.com>
2022-03-17 13:28:55 -0700

    changed get and post

--- server.js
+++ server.js
@@ -1,47 +1,135 @@
 const express = require('express')
 let notes = require('./db/db.json')
-const path = require('path')
-const fs = require('fs') // do I need this?
+const { join } = require('path')
+const { writeFile, readFile } = require('fs') // do I need this?
 const { uid } = require('uid') // deconstructed?
 
 const app = express()
 
 // boiler plate
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 // access to index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile(join(__dirname, 'public', 'index.html'))
 })
 
 // access to notes.html
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+  res.sendFile(join(__dirname, 'public', 'notes.html'))
 })
 
-// get the notes
+// get the notes from the DB
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  readFile(join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) { console.log(err) }
+    res.json(JSON.parse(data))
+  })
 })
 
-// post a new note
+// post new notes
 app.post('/api/notes', (req, res) => {
-  let newNote = { // from index.js
-    title: req.body.title,
-    text: req.body.text,
-    id: uid()
+  // console.log(req.rawHeaders)
+  // console.log(req.method)
+  if (req.body.title.length < 1) {
+    return res.json({})
+  } else {
+    if (!req.body.title) {
+      req.body.title = ''
+    }
+    readFile(join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+      if (err) { console.log(err) }
+      const notes = JSON.parse(data)
+      notes.push(req.body)
+      writeFile(join(__dirname, 'db', 'db.json'), JSON.stringify(notes), err => {
+        if (err) { console.log(err) }
+        res.json(req.body)
+      })
+    })
   }
-  notes.push(newNote)
-  res.json(200)
 })
 
 // delete a note
 app.delete('/api/notes/:id', (req, res) => {
-  notes = notes.filter(note => note.id !== req.params.id)
-  res.json(notes)
+  readFile(join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) { console.log(err) }
+    notes = notes.filter(note => note.id !== req.param.id)
+    writeFile(join(__dirname, 'db', 'db.json'), JSON.stringify(notes), err => {
+      if (err) { console.log(err) }
+      res.json(req.body)
+    })
+  })
+  // res.json(notes)
 })
 
 // listen on port 3000
 app.listen(process.env.PORT || 3000)
+
+
+// code from class -------------------------------------------
+// const { writeFile, readFile } = require('fs')
+// const express = require('express')
+// const { join } = require('path')
+
+// const app = express()
+
+// app.use(express.static(join(__dirname, 'public')))
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
+
+// // app.get('/contact', (req, res) => {
+// //   res.sendFile(join(__dirname, 'public', 'contact.html'))
+// // })
+
+// // app.get('/test', (req, res) => {
+// //   res.send('<button>It is working!</button>')
+// // })
+
+// // const person = {
+// //   name: 'John Doe',
+// //   age: 40,
+// //   email: 'johndoe@gmail.com'
+// // }
+
+// // app.get('/person', (req, res) => {
+// //   res.json(person)
+// // })
+
+// // role -> boss, manager, employee, intern
+// // app.get('/position/:role', (req, res) => {
+// //   const position = roles.filter(role => role.title.toLowerCase() === req.params.role.toLowerCase())[0]
+// //   res.json(position)
+// // })
+
+// app.get('/positions', (req, res) => {
+//   readFile(join(__dirname, 'db', 'positions.json'), 'utf8', (err, data) => {
+//     if (err) { console.log(err) }
+//     res.json(JSON.parse(data))
+//   })
+// })
+
+// app.post('/positions', (req, res) => {
+//   // console.log(req.rawHeaders)
+//   // console.log(req.method)
+//   if (req.body.title.length < 1) {
+//     return res.json({})
+//   } else {
+//     if (!req.body.salary) {
+//       req.body.salary = 0
+//     }
+//     readFile(join(__dirname, 'db', 'positions.json'), 'utf8', (err, data) => {
+//       if (err) { console.log(err) }
+//       const positions = JSON.parse(data)
+//       positions.push(req.body)
+//       writeFile(join(__dirname, 'db', 'positions.json'), JSON.stringify(positions), err => {
+//         if (err) { console.log(err) }
+//         res.json(req.body)
+//       })
+//     })
+//     // writeFile('')
+//   }
+// })
+
+// app.listen(3000)
